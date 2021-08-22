e76284e0e0ae3ce78730940664c2b4e133e52172
caroacostatovany <carolina.acostatovany@gmail.com>
2021-08-22 11:43:21 -0500

    bug fix

--- src/busquedadesaparecidos/ner/busqueda_expediente.py
+++ src/busquedadesaparecidos/ner/busqueda_expediente.py
@@ -5,11 +5,12 @@ def encontrar_expediente(texto):
     x = re.search("xp[0-9,=,-]+", texto)
 
     try:
-        x.group()
+        x = x.group()
     except:
         x = "No encontrado"
 
     return x
+
 
 def convertir_texto_exp_dataframe(path, expediente, texto):
     texto_exp = "{}| {}".format(expediente, texto.replace(expediente, ''))
